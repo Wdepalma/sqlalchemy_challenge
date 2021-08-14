e7eb4a1b9dfbc2a365d3dc2cde26bbc5847ce3a4
Wdepalma <wdepalma@verizon.net>
2021-08-13 22:03:37 -0400

    intermediate progress

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
+#Database set-up:
 
-app = Flask(__Climate__)
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 
+app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return "Hi"
 
 @app.route("/")
 def Climate_Home_Page():
@@ -24,27 +38,40 @@ def Climate_Home_Page():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    return 
-    
-    #hello_dict
+    session = Session(engine)
+
+    precips = session.query(measurement.date, measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary 
+    prcp_list=[]
+    for date, prcp in precips:
+        rain_dict = {}
+        rain_dict["date"] = date
+        rain_dict["prcp"] = prcp
+        prcp_list.append(rain_dict)    
+
+    return jsonify(prcp_list)
 
 
-@app.route("/api/v1.0/stations")
-def stations():
-    return jsonify(hello_dict)
 
-@app.route("/api/v1.0/tobs")
-def tobs():
-    return jsonify(hello_dict)
+#@app.route("/api/v1.0/stations")
+#def stations():
+#    return jsonify(hello_dict)
 
-@app.route("/api/v1.0/<start>")
-def from_when(start):
-    return jsonify(hello_dict)
+#@app.route("/api/v1.0/tobs")
+#def tobs():
+#    return jsonify(hello_dict)
+
+#@app.route("/api/v1.0/<start>")
+#def from_when(start):
+#    return jsonify(hello_dict)
 
 
-@app.route("/api/v1.0/<start>/<end>")
-def range(start, end):
-    return jsonify(hello_dict)
+#@app.route("/api/v1.0/<start>/<end>")
+#def range(start, end):
+#    return jsonify(hello_dict)
 
 
 if __name__ == "__main__":
